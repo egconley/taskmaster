2cff6b74929c298faddc63c04072e3e2cb4a20aa
egconley <31393715+egconley@users.noreply.github.com>
2020-02-11 11:43:34 -0800

    MainActivity, AllTasks, AddATask

---
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAddTask = new Intent(MainActivity.this, AddATask.class);
+                startActivity(goToAddTask);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAllTasks = new Intent(MainActivity.this, AllTasks.class);
+                startActivity(goToAllTasks);
+            }
+        });
+    }
+}
