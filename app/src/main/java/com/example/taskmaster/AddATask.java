665ed0979028191b0a54786888957ffa41461f57
egconley <31393715+egconley@users.noreply.github.com>
2020-02-11 11:54:03 -0800

    comment

--- app/src/main/java/com/example/taskmaster/AddATask.java
+++ app/src/main/java/com/example/taskmaster/AddATask.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class AddATask extends AppCompatActivity {
 
+    //comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
