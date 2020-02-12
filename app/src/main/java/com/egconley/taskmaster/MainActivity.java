269af118bc38b78074f45e08b789e3230f97ea8d
egconley <31393715+egconley@users.noreply.github.com>
2020-02-12 12:23:08 -0800

    settings activity includes username EditText, main page updates to include username when not null

--- app/src/main/java/com/egconley/taskmaster/MainActivity.java
+++ app/src/main/java/com/egconley/taskmaster/MainActivity.java
@@ -3,11 +3,17 @@ package com.egconley.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.Iterator;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton settingsButton = findViewById(R.id.settingsButton);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToSettings = new Intent(MainActivity.this, Settings.class);
+
+                startActivity(goToSettings);
+            }
+        });
 
         Button addTaskButton = findViewById(R.id.addTaskButton);
 
@@ -82,4 +99,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "resumed");
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String username = sharedPreferences.getString("username", null);
+
+        TextView myTasksHeader = findViewById(R.id.myTasksHeader);
+
+        String myTasksHeaderText = username + "\'s Tasks";
+
+        if (username!=null) {
+            myTasksHeader.setText(myTasksHeaderText);
+        }
+
+    }
+
 }
--- app/src/main/java/com/egconley/taskmaster/Settings.java
+++ app/src/main/java/com/egconley/taskmaster/Settings.java
@@ -2,13 +2,65 @@ package com.egconley.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
+
+    String TAG = "egc.settings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ImageButton saveUsernameButton = findViewById(R.id.saveUsername);
+
+        saveUsernameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences p =
+                        PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor = p.edit();
+
+                EditText enterUsername = findViewById(R.id.enterUsername);
+                String username = enterUsername.getText().toString();
+
+                editor.putString("username", username);
+                editor.apply();
+
+                Toast toast = Toast.makeText(Settings.this, "Saved!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "resumed");
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        String username = sharedPreferences.getString("username", null);
+
+        EditText enterUsername = findViewById(R.id.enterUsername);
+
+        if (username!=null) {
+            enterUsername.setText(username);
+        }
+
     }
 }
