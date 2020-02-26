1b00e72ba6791b828aa9a6f1b8c62a808483cecf
egconley <31393715+egconley@users.noreply.github.com>
2020-02-26 13:11:01 -0800

    added spinner to addATask activity that populates with teams from database

--- app/src/main/java/com/egconley/taskmaster/AddATask.java
+++ app/src/main/java/com/egconley/taskmaster/AddATask.java
@@ -1,33 +1,43 @@
 package com.egconley.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.amazonaws.amplify.generated.graphql.CreateTaskMutation;
-import com.amazonaws.amplify.generated.graphql.CreateTeamMutation;
-import com.amazonaws.amplify.generated.graphql.CreateTeamMutation;
+import com.amazonaws.amplify.generated.graphql.ListTeamsQuery;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 import com.apollographql.apollo.GraphQLCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 import javax.annotation.Nonnull;
 
 import type.CreateTaskInput;
-import type.CreateTeamInput;
 
 public class AddATask extends AppCompatActivity {
 
     String TAG = "egc.addATask";
 
     private AWSAppSyncClient mAWSAppSyncClient;
+    RecyclerView recyclerView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +49,44 @@ public class AddATask extends AppCompatActivity {
                 .awsConfiguration(new AWSConfiguration(getApplicationContext()))
                 .build();
 
-        Button button3 = findViewById(R.id.button3);
+        final Spinner spinner = findViewById(R.id.spinner);
+        final String[] spinnerTeams = new String[3];
+        ListTeamsQuery teams = ListTeamsQuery.builder().build();
+        final HashMap<String, String> teamHashmap = new HashMap<>();
+        mAWSAppSyncClient.query(teams).enqueue(new GraphQLCall.Callback<ListTeamsQuery.Data>() {
+            @Override
+            public void onResponse(@Nonnull Response<ListTeamsQuery.Data> response) {
+                int idx = 0;
+                for (ListTeamsQuery.Item team : response.data().listTeams().items()) {
+                    teamHashmap.put(team.name(), team.id());
+                    spinnerTeams[idx] = team.name();
+                    idx++;
+                }
+                
+                adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, spinnerTeams);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                Handler threadHandler = new Handler(Looper.getMainLooper()) {
+                    public void handleMessage(Message msg) {
+                        spinner.setAdapter(adapter);
+                    }
+                };
+                threadHandler.obtainMessage().sendToTarget();
+            }
+
+            @Override
+            public void onFailure(@Nonnull ApolloException e) {
+                Log.i("ran", "failed to pull from db");
+            }
+        });
+
+        Button button3 = findViewById(R.id.addTaskButton);
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText newTaskTitle = findViewById(R.id.addTaskName);
                 EditText newTaskBody = findViewById(R.id.addTaskBody);
-
-
 
                 CreateTaskInput input = CreateTaskInput.builder()
                         .title(newTaskTitle.getText().toString())
